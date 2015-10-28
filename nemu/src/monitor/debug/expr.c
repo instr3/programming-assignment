adeb25a7d4376987d23a14b541afce415b0bd25e
tracer-ics2015 <tracer@njuics.org>
2015-10-28 19:15:44 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  19:15:44 up 10:18,  2 users,  load average: 0.00, 0.04, 0.10 3becd53c1779109f9005bf60f5b38d0acc02ad50

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -287,9 +287,9 @@ uint32_t expr(char *e, bool *success)
 	if (errorCode)
 	{
 		success = false;
-		if (errorCode == MATH_ERROR)Log("\33[1;31Math error");
-		if (errorCode == SYNTAX_ERROR)Log("\33[1;31Syntax error");
-		if (errorCode == MEMORY_ACCESS_ERROR)Log("\33[1;31Memory access error");
+		if (errorCode == MATH_ERROR)printf("\33[1;31mMath error");
+		if (errorCode == SYNTAX_ERROR)printf("\33[1;31mSyntax error");
+		if (errorCode == MEMORY_ACCESS_ERROR)printf("\33[1;31mMemory access error");
 		printf(" occured in expr \"%s\" near '", e);
 		int i;
 		for (i = errorFrom; i <= errorTo; ++i)putchar(e[i]);
