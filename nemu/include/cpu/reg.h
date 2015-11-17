f03176a8cb94276e60bdec978cb4b1d98d5d633c
tracer-ics2015 <tracer@njuics.org>
2015-11-17 16:57:15 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:57:15 up  6:33,  2 users,  load average: 0.98, 0.60, 0.50 35a53ff83c00c9517579cc2615def2a38bcee940

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -65,12 +65,12 @@ static inline int check_reg_index(int index) {
 #define EFLAGS_OF 11
 #define highestbit(res) ((res)&1ull<<8*DATA_BYTE)
 
-#define influence_zf(res) reg_flag_set(EFLAGS_ZF,res)
+#define influence_zf(res) reg_flag_set(EFLAGS_ZF,!res)
 #define influence_pf(res) reg_flag_set(EFLAGS_PF,~__builtin_popcount(res)&1)
 #define influence_sf(res) reg_flag_set(EFLAGS_SF,highestbit(res))
 #define influence_of(res,op1,op2) reg_flag_set(EFLAGS_OF,highestbit(op1)==highestbit(op2)&& \
 	highestbit(op1)!=highestbit(res))
-#define influence_cf(res,resull) reg_flag_set(EFLAGS_CF,res==resull)
+#define influence_cf(res,resull) reg_flag_set(EFLAGS_CF,res!=resull)
 /*Bit Name Function page 419
 0 CF Carry Flag ── Set on high-order bit carry or borrow; cleared
 otherwise.
