6b25b5d309412169820753dd8c44156973b4415d
tracer-ics2015 <tracer@njuics.org>
2015-11-17 19:20:35 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  19:20:35 up  8:56,  3 users,  load average: 0.33, 0.47, 0.49 17446c13c396e1c2eca32d4eab373e4ad98c71

--- nemu/src/cpu/exec/narrative/call-template.h
+++ nemu/src/cpu/exec/narrative/call-template.h
@@ -6,7 +6,7 @@
 make_helper(concat(call_i_, SUFFIX)) {
 	swaddr_t addr = instr_fetch(eip + 1, 4);
 	push_data(cpu.eip,4);
-	cpu.eip+=5+addr;
+	cpu.eip+=addr;
 
 	print_asm("call 0x%x", cpu.eip);
 	return 5;
