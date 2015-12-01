e62a34b52acf9e2b06aed89cf9a3d02c082152ca
tracer-ics2015 <tracer@njuics.org>
2015-12-01 14:20:40 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  14:20:40 up 1 day,  3:19,  2 users,  load average: 0.09, 0.26, 0.30 e262231ff297e59538c53af371e96423b906a547

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -5,14 +5,14 @@
 static void do_execute () {
 	DATA_TYPE result = op_dest->val - op_src->val;
 	printf("Look:%u\n",result);
-	printf("Look2:%llu\n",op_dest->val+0ull-op_src->val);
+	printf("Look2:%llu\n",op_dest->val+1ull+(~op_src->val));
 	//printf("Highest Bit:%llu,%llu\n",1ull<<8*DATA_BYTE,result&1ull<<8*DATA_BYTE);
 	//OPERAND_W(op_dest, result);
 	influence_zf(result);
 	influence_pf(result);
 	influence_sf(result);
 	influence_of(result,op_dest->val,~op_src->val);
-	influence_cf(result,op_dest->val+0ull-op_src->val,1);
+	influence_cf(result,op_dest->val+1ull+(~op_src->val),1);
 
 	print_asm_template2();
 }
--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -9,7 +9,7 @@ static void do_execute () {
 	influence_pf(result);
 	influence_sf(result);
 	influence_of(result,op_dest->val,~op_src->val);
-	influence_cf(result,op_dest->val+0ull-op_src->val,1);
+	influence_cf(result,op_dest->val+1ull+(~op_src->val),1);
 
 	print_asm_template2();
 }
