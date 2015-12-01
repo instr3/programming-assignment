5c7f0835fc4eabebd052070a795a1f52fe3864f5
tracer-ics2015 <tracer@njuics.org>
2015-12-01 14:50:08 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  14:50:08 up 1 day,  3:48,  2 users,  load average: 0.63, 0.40, 0.34 ee1b1de078fd7f4834fed7514166b3ee4c3482bc

--- nemu/src/cpu/exec/data-mov/lea-template.h
+++ nemu/src/cpu/exec/data-mov/lea-template.h
@@ -1,13 +1,13 @@
 #include "cpu/exec/template-start.h"
 
 #define instr lea
-#if DATA_BYTE == 2 || DATA_BYTE ==4
+//#if DATA_BYTE == 2 || DATA_BYTE ==4
 static void do_execute() {
 	OPERAND_W(op_dest, op_src->val);
 	print_asm_template2();
 }
 
 make_instr_helper(rm2r)
-#endif
+//#endif
 
 #include "cpu/exec/template-end.h"
