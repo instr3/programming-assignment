48768fdcd60a9f6ed8f89a4499e5cfb48d46d1a1
tracer-ics2015 <tracer@njuics.org>
2016-01-23 21:51:25 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  21:51:25 up  3:31,  2 users,  load average: 0.73, 0.57, 0.43 dbb9e2fe2838ab62d0f4d2d02dab953ce974cd90

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -17,6 +17,7 @@
 #include "arith/idiv.h"
 #include "arith/div.h"
 #include "arith/add.h"
+#include "arith/adc.h"
 #include "arith/sub.h"
 #include "arith/sbb.h"
 #include "arith/cmp.h"
---
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -0,0 +1,25 @@
+#include "cpu/exec/template-start.h"
+
+#define instr adc
+
+static void do_execute () {
+	DATA_TYPE result = op_dest->val + op_src->val + reg_flag(EFLAGS_CF);
+	OPERAND_W(op_dest, result);
+	influence_zf(result);
+	influence_pf(result);
+	influence_sf(result);
+	influence_of(result,op_dest->val,op_src->val);
+	influence_cf(result,op_dest->val+0ull+op_src->val+reg_flag(EFLAGS_CF),0);
+
+	print_asm_template2();
+}
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
