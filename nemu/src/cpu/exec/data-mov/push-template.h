41d494a11f919337131e04207937402679ed3a4c
tracer-ics2015 <tracer@njuics.org>
2016-01-23 16:56:24 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:56:24 up  1:50,  2 users,  load average: 0.46, 0.34, 0.38 2a0d04e8d13765129035c30a2e824b4df68544a

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -15,6 +15,7 @@ static void do_execute() {
 	print_asm_template1();
 }
 #if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(rm)
 make_instr_helper(r)
 #endif
 make_instr_helper(i)
--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -80,7 +80,7 @@ make_group(group4,
 /* 0xff */
 make_group(group5,
 	inv, inv, call_rm_v, inv, 
-	jmp_rm_v, inv, inv, inv)
+	jmp_rm_v, inv, push_rm_v, inv)
 
 make_group(group6,
 	inv, inv, inv, inv, 
@@ -120,7 +120,7 @@ helper_fun opcode_table [256] = {
 /* 0x5c */	inv, inv, inv, inv,
 /* 0x60 */	inv, inv, inv, inv,
 /* 0x64 */	inv, inv, data_size, inv,
-/* 0x68 */	inv, imul_i_rm2r_v, inv, imul_si_rm2r_v,
+/* 0x68 */	push_i_v, imul_i_rm2r_v, push_i_b, imul_si_rm2r_v,
 /* 0x6c */	inv, inv, inv, inv,
 /* 0x70 */	jcc_b, jcc_b, jcc_b, jcc_b,
 /* 0x74 */	jcc_b, jcc_b, jcc_b, jcc_b,
