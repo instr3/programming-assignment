2b039486105f8c899a617eb908fc253fd3104ae7
tracer-ics2015 <tracer@njuics.org>
2016-01-24 13:14:10 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  13:14:10 up  7:55,  2 users,  load average: 0.36, 0.16, 0.18 5ef07b372269704343b5e8ccf928b26ddc2122

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -3,7 +3,7 @@
 
 make_helper(concat(movs_, SUFFIX)) {
 	MEM_W(REG(R_EDI),MEM_R(REG(R_ESI)));
-	printf("%d\n",MEM_R(REG(R_ESI)));
+	printf("%d\n",REG(R_ESI));
 	REG(R_EDI)+=DATA_BYTE;
 	REG(R_ESI)+=DATA_BYTE;
 	print_asm("movs" str(SUFFIX));
