ae00187e3a4dc2424a53c9aec8fcc1a6bcbed432
tracer-ics2015 <tracer@njuics.org>
2016-01-24 13:13:36 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  13:13:36 up  7:54,  2 users,  load average: 0.27, 0.12, 0.17 10e01b946fe89cd9d84fb9d9f58e9e7a494395d2

--- nemu/src/cpu/exec/string/stos-template.h
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -3,7 +3,7 @@
 
 make_helper(concat(stos_, SUFFIX)) {
 	MEM_W(REG(R_EDI),MEM_R(REG(R_EAX)));
-	assert(MEM_R(REG(R_EAX))!=0);
+	printf("%d\n",MEM_R(REG(R_EAX)));
 	REG(R_EDI)+=DATA_BYTE;
 	print_asm("stos" str(SUFFIX));
 	return 0;
