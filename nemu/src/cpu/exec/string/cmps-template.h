32cc36d94d50b2d99d22aa6ab7c18f284d144d99
tracer-ics2015 <tracer@njuics.org>
2016-01-25 16:58:47 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:58:47 up  8:57,  2 users,  load average: 0.47, 0.41, 0.31 55f4afa3184da8a87a8edcad81cbe665769a1ad7

--- nemu/src/cpu/exec/string/cmps-template.h
+++ nemu/src/cpu/exec/string/cmps-template.h
@@ -1,9 +1,15 @@
 #include "cpu/exec/template-start.h"
 
 make_helper(concat(cmps_, SUFFIX)) {
+	DATA_TYPE result=MEM_R(reg_l(R_ESI))-MEM_R(reg_l(R_EDI));
+	influence_zf(result);
+	influence_pf(result);
+	influence_sf(result);
+	influence_of(result,MEM_R(reg_l(R_ESI)),~MEM_R(reg_l(R_EDI)));
+	influence_cf(result,MEM_R(reg_l(R_ESI))+1ull+(~MEM_R(reg_l(R_EDI))),1);
+
 	reg_l(R_EDI)+=DATA_BYTE;
 	reg_l(R_ESI)+=DATA_BYTE;
-	influence_zf(MEM_R(reg_l(R_ESI))-MEM_R(reg_l(R_EDI)));
 	print_asm("cmps" str(SUFFIX));
 	return 1;
 }
