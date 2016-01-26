5467e10865184a22909c9ea0ab9176ac5b98e731
tracer-ics2015 <tracer@njuics.org>
2016-01-26 15:24:28 +0800

    >  compile kernel 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  15:24:28 up 17:17,  2 users,  load average: 0.54, 0.36, 0.33 4d0cf97a3b9306e2b9ac76735eea31e72b32226c

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -42,8 +42,8 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			//memcpy((void *)(ph->p_vaddr),(void *)(ph->p_paddr),ph->p_filesz);
-			//memset((void *)(ph->p_vaddr+ph->p_filesz),0,ph->p_memsz-ph->p_filesz);
+			memcpy((void *)(ph->p_vaddr),(void *)(ph->p_paddr),ph->p_filesz);
+			memset((void *)(ph->p_vaddr+ph->p_filesz),0,ph->p_memsz-ph->p_filesz);
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
