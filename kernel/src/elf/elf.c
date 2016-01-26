8844c9d40f4d77e31033c48e95b57f792a67e6ee
tracer-ics2015 <tracer@njuics.org>
2016-01-26 16:04:19 +0800

    >  compile kernel 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:04:19 up 17:56,  2 users,  load average: 1.02, 0.39, 0.43 b64c98efa406db94bcc2c26707e9534c12a7e15

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -39,7 +39,6 @@ uint32_t loader() {
 	for(ph=(void *)(buf+elf->e_phoff); i<elf->e_phnum ; ++i,++ph) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
-			set_bp();
 
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
