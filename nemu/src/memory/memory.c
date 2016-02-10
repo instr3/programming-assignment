a6cb034f95d29a4cdc050e6d4fa5065ebfcd95d0
tracer-ics2015 <tracer@njuics.org>
2016-02-10 16:26:51 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:26:51 up 1 day, 22:15,  4 users,  load average: 0.00, 0.01, 0.05 b5d30ecfc39a0dc876dd6fd41884ff87cd198a3

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -6,18 +6,62 @@ void dram_write(hwaddr_t, size_t, uint32_t);
 /* Memory accessing interfaces */
 
 
-typedef struct cacheblock
+/*typedef struct cacheblock
 {
-	bool valid;
+	uint32_t valid;
 	uint32_t data[];//Size decided by malloc
-}cacheblock_t;
+}cacheblock_t;*/
 
 typedef struct cache
 {
+	uint32_t mem_width, mem_size;//Memory size
+	uint32_t block_width, block_size;//Cache block size
+	uint32_t bid_width, bid_num;//Block ID count
+	uint32_t way_num;//associative ways count
+	uint8_t ***blocks;//blocks[block_id][way]
+	bool **valid;
 
 }cache_t;
 cache_t cacheV1, cacheV2;//V1 and V2 cache
+void init_cache(cache_t cache, int mem_width/*= 27*/, int block_width, int bid_width, int way_num)//Memory address size, Block size, Block ID size
+{
+	//init parameters
+	cache.mem_width=mem_width;
+	cache.block_width=block_width;
+	cache.bid_width=bid_width;
 
+	cache.mem_size=1<<mem_width;
+	cache.block_size=1<<block_width;
+	cache.bid_num=1<<bid_width;
+	cache.way_num=way_num;
+
+	//Allocate cache blocks
+	int i,j;
+	cache.blocks=malloc(cache.bid_num * sizeof(uint8_t **));
+	for(i=0;i<cache.bid_num;++i)
+	{
+		cache.blocks[i]=malloc(cache.way_num * sizeof(uint8_t *));
+		for(j=0;j<way_num;++j)
+		{
+			cache.blocks[i][j]=malloc(cache.block_size * sizeof(uint8_t));
+			cache.valid[i][j]=false;
+		}
+	}
+}
+uint32_t cache_read(cache_t cache,hwaddr_t addr, size_t len, bool *success)
+{
+	return 0;
+	//uint32_t addr_offset=addr & (cache.block_size - 1);
+	//uint32_t addr_bid=(addr>>(cache.mem_width - cache.bid_width));
+
+	//int j;
+	//for(j=0)
+}
+
+void init_cache_groups()
+{
+	//init_cache(cacheV1, 27, 6, 16 - 6 - 3);//64B per block, cache size 64KB, 8-way.
+}
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
