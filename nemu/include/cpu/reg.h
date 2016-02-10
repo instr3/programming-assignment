e0f8d0281f50d9bca0fc1121cd90bbfb3d37eea2
tracer-ics2015 <tracer@njuics.org>
2016-02-10 16:36:23 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:36:23 up 1 day, 22:25,  4 users,  load average: 0.12, 0.07, 0.06 60e9e401bfd8a05ed71fe770ca5e0f7aed31bc2b

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -85,8 +85,8 @@ positive, 1 if negative).
 11 OF Overflow Flag ── Set if result is too large a positive number
 or too small a negative number (excluding sign-bit) to fit in
 destination operand; cleared otherwise.*/
-#define push_data(word,len) do{cpu.esp-=len;swaddr_write(cpu.esp,len,word);}while(0)
-#define pop_data(word,len) do{word=swaddr_read(cpu.esp,len);cpu.esp+=len;}while(0)
+#define push_data(word,len) do{cpu.esp-=(len);swaddr_write(cpu.esp,len,word);}while(0)
+#define pop_data(word,len) do{word=swaddr_read(cpu.esp,len);cpu.esp+=(len);}while(0)
 #define string_advance(word,step) (reg_flag(EFLAGS_DF)?((word)-=(step)):((word)+=(step)))
 
 extern const char* regsl[];
