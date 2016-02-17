207cc0e184ce7276a8a325ea96b7978b358f7c2d
tracer-ics2015 <tracer@njuics.org>
2016-02-17 22:51:58 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  22:51:58 up  4:05,  2 users,  load average: 0.26, 0.12, 0.09 d7168f875bbb40201e394dc6646b60f1850e2358

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -98,7 +98,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 #ifdef USE_CACHE
 	int result=cache1.read(&cache1,addr,len) & (~0u >> ((4 - len) << 3));
 	printf("RES:%x,%x",result,dram_read(addr, len) & (~0u >> ((4 - len) << 3)));
-	exit(0);
+
 	//assert(result==dram_read(addr, len));
 	return result;
 #else
