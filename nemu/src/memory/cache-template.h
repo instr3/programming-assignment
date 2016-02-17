94c7bc64ee20c03ff1f1f34540f718552eb2f29c
tracer-ics2015 <tracer@njuics.org>
2016-02-17 23:40:40 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  23:40:40 up  4:53,  2 users,  load average: 0.10, 0.09, 0.06 b9f118545d24c0554e437caddac4a0024844281

--- nemu/src/memory/cache-template.h
+++ nemu/src/memory/cache-template.h
@@ -110,7 +110,7 @@ uint32_t concat(CACHE_ID,read)(struct CACHE_T *this,hwaddr_t addr, size_t len) {
 		int more=cache_offset + len - OFFSET_LEN;
 		//this->cache_read_raw(addr + 4, temp + 4, ch);
 		printf("[%x %x %x %x]\n",temp[0],temp[1],temp[2],temp[3]);
-		memcpy(temp + more, &ch->block[addr & OFFSET_MASK],4 - more);
+		memcpy(temp + 4 - more, &ch->block[0],more);
 		printf("[%x %x %x %x]%d\n",temp[0],temp[1],temp[2],temp[3],OFFSET_LEN - cache_offset);
 	
 	}
