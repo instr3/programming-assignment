250c6be8d868e28b1b9a717d1a2df3847e6a5c45
tracer-ics2015 <tracer@njuics.org>
2016-02-18 23:48:37 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  23:48:37 up 11:40,  2 users,  load average: 0.07, 0.06, 0.05 836e57027932f71d2cda1750b6e03ee412204808

--- nemu/src/cpu/exec/segment/lgtd.c
+++ nemu/src/cpu/exec/segment/lgtd.c
@@ -1,12 +1,13 @@
 #include "cpu/exec/helper.h"
 
 make_helper(lgtd) {
-	cpu.gdtr_limit=swaddr_read(cpu.eip+2,2);
-	cpu.gdtr_base=swaddr_read(cpu.eip+4,4);
+	swaddr_t addr=swaddr_read(cpu.eip+3,4);
+	cpu.gdtr_limit=swaddr_read(addr,2);
+	cpu.gdtr_base=swaddr_read(addr+2,4);
 	if(ops_decoded.is_data_size_16)
 	{
 		cpu.gdtr_base&=0xFFFFFF;
 	}
 	print_asm("lgtd");
-	return 7;
+	return 6;
 }
