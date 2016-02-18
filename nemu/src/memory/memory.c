adcb3fa0c31a454aaa2a8b84862c29301862e419
tracer-ics2015 <tracer@njuics.org>
2016-02-18 11:16:08 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  11:16:08 up  6:07,  2 users,  load average: 0.01, 0.05, 0.05 592cfa718ada17884f97d7ee49d9f246963b1749

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -34,10 +34,8 @@ void dram_write(hwaddr_t, size_t, uint32_t);
 	#define WAY_NUM 8
 	#define CACHE_ID cache1
 	//Define the next level cache
-	//#define slower_read(...) cache2.read(&cache2,__VA_ARGS__)
-	//#define slower_write(...) cache2.write(&cache2,__VA_ARGS__)
-	#define slower_read(...) dram_read(__VA_ARGS__)
-	#define slower_write(...) dram_write(__VA_ARGS__)
+	#define slower_read(...) cache2.read(&cache2,__VA_ARGS__)
+	#define slower_write(...) cache2.write(&cache2,__VA_ARGS__)
 
 	#include "cache-template.h"
 
