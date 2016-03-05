6863027456bce2c965c9078f235361e9e6f060b0
tracer-ics2015 <tracer@njuics.org>
2016-03-05 23:16:05 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  23:16:05 up 1 day, 13:56,  2 users,  load average: 0.03, 0.09, 0.13 bec1922d5bc30423a224c8724eeb0c9f3911931

--- nemu/src/cpu/exec/data-mov/mov2.c
+++ nemu/src/cpu/exec/data-mov/mov2.c
@@ -4,3 +4,16 @@
 #include "mov2-template.h"
 #undef DATA_BYTE
 
+make_helper(mov2_set_seg)
+{
+	uint8_t tmp = instr_fetch(eip + 1, 1);
+	char *segname;
+	switch(tmp)
+	{
+		case 0xd8:cpu.ds=cpu.eax;segname="ds";break;
+		case 0xc0:cpu.es=cpu.eax;segname="es";break;
+		case 0xd0:cpu.ss=cpu.eax;segname="ss";break;
+	}
+	print_asm("mov %%eax,%%%s",segname);
+	return 2;
+}
--- nemu/src/cpu/exec/data-mov/mov2.h
+++ nemu/src/cpu/exec/data-mov/mov2.h
@@ -2,5 +2,5 @@
 #define __MOV2_H__
 
 make_helper(mov2_rm_l);
-
+make_helper(mov2_set_seg);
 #endif
