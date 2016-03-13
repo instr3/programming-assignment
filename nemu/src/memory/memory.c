6e98857e1c2543db80d5e2820a9ddb74e7dc9af3
tracer-ics2015 <tracer@njuics.org>
2016-03-13 11:03:49 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  11:03:49 up 2 days,  2:57,  2 users,  load average: 0.05, 0.03, 0.05 58988500242db1e8b21d3ed506b3027d0355f18

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -5,61 +5,60 @@
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
+#ifdef USE_CACHE
+
+//Create Second Level Cache
+#define OFFSET_BITS 6
+#define BID_BITS 12 //Block ID Bits = 4MB/16B/16Ways
+#define WAY_NUM 16
+#define CACHE_WRITE_BACK_AND_WRITE_TRHOUGH //Define the Write_back feature
+#define CACHE_ID cache2
+//Define the next level cache
+#define slower_read(...) dram_read(__VA_ARGS__)
+#define slower_write(...) dram_write(__VA_ARGS__)
+
+#include "cache-template.h"
+
+#undef OFFSET_BITS
+#undef BID_BITS
+#undef WAY_NUM
+#undef CACHE_WRITE_BACK_AND_WRITE_TRHOUGH
+#undef CACHE_ID
+#undef slower_read
+#undef slower_write
+
+//Create First Level Cache(Modified)
+#define OFFSET_BITS 6
+#define BID_BITS 7 
+#define WAY_NUM 8
+#define CACHE_ID cache1
+//Define the next level cache
+#define slower_read(...) cache2.read(&cache2,__VA_ARGS__)
+#define slower_write(...) cache2.write(&cache2,__VA_ARGS__)
+
+#include "cache-template.h"
+
+#undef OFFSET_BITS
+#undef BID_BITS
+#undef WAY_NUM
+#undef CACHE_ID
+#undef slower_read
+#undef slower_write
+
+void init_caches()
+{
+	cache1_init(&cache1);
+	cache2_init(&cache2);
+}
+void debug_cache_address(hwaddr_t addr)
+{
+	cache1.debug(&cache1,addr);
+	cache2.debug(&cache2,addr);
+}
+#endif
+
 /* Memory accessing interfaces */
 
-#ifdef USE_CACHE
-//{
-	//Create Second Level Cache
-	#define OFFSET_BITS 6
-	#define BID_BITS 12 //Block ID Bits = 4MB/16B/16Ways
-	#define WAY_NUM 16
-	#define CACHE_WRITE_BACK_AND_WRITE_TRHOUGH //Define the Write_back feature
-	#define CACHE_ID cache2
-	//Define the next level cache
-	#define slower_read(...) dram_read(__VA_ARGS__)
-	#define slower_write(...) dram_write(__VA_ARGS__)
-
-	#include "cache-template.h"
-
-	#undef OFFSET_BITS
-	#undef BID_BITS
-	#undef WAY_NUM
-	#undef CACHE_WRITE_BACK_AND_WRITE_TRHOUGH
-	#undef CACHE_ID
-	#undef slower_read
-	#undef slower_write
-
-	//Create First Level Cache(Modified)
-	#define OFFSET_BITS 6
-	#define BID_BITS 7 
-	#define WAY_NUM 8
-	#define CACHE_ID cache1
-	//Define the next level cache
-	#define slower_read(...) cache2.read(&cache2,__VA_ARGS__)
-	#define slower_write(...) cache2.write(&cache2,__VA_ARGS__)
-
-	#include "cache-template.h"
-
-	#undef OFFSET_BITS
-	#undef BID_BITS
-	#undef WAY_NUM
-	#undef CACHE_ID
-	#undef slower_read
-	#undef slower_write
-
-	void init_caches()
-	{
-		cache1_init(&cache1);
-		cache2_init(&cache2);
-	}
-
-	void debug_cache_address(hwaddr_t addr)
-	{
-		cache1.debug(&cache1,addr);
-		cache2.debug(&cache2,addr);
-	}
-//}
-#endif
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 #ifdef USE_CACHE
 	return cache1.read(&cache1,addr,len) & (~0u >> ((4 - len) << 3));
