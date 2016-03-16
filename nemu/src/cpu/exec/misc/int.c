6edf5621c8af6776d60fd8a9ca9282d7affc533c
tracer-ics2015 <tracer@njuics.org>
2016-03-16 15:43:30 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  15:43:30 up 2 days,  8:15,  2 users,  load average: 0.19, 0.15, 0.11 44e1be2cd42ce90bdef57efb39ef2e4c96697711

--- nemu/src/cpu/exec/misc/int.c
+++ nemu/src/cpu/exec/misc/int.c
@@ -11,6 +11,8 @@ void raise_intr(uint8_t NO) {
 	
 	idtitem_t gate;
 	lnaddr_t address=(uint32_t)NO*8+cpu.idtr_base;
+	printf("%x\n",address);
+	fflush(stdout);
 	assert(address<cpu.idtr_limit+cpu.idtr_base);
 	gate.item=lnaddr_read(address,4)+((uint64_t)lnaddr_read(address+4,4)<<32);
 	uint32_t offset=gate.offset_15_0+(gate.offset_31_16<<16);
