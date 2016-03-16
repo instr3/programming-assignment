153801f89c224fc21ddd8d39ac1376628862ef3d
tracer-ics2015 <tracer@njuics.org>
2016-03-16 20:00:44 +0800

    >  compile kernel 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  20:00:44 up 2 days, 11:25,  2 users,  load average: 0.10, 0.14, 0.14 29e5937cb679239012762ecc232d99c8b6376592

--- testcase/src/hello.c
+++ testcase/src/hello.c
@@ -59,12 +59,10 @@ int isatty(int fd) {
 	return 0;
 }
 
-char buf[128];
 int main() {
 	set_bp();
 	printf("%s", "Hello world!\n");
-	sprintf(buf,"%d + %d = %d\n", 1, 1, 2);
-	printf("%s", buf);
+	printf("%d + %d = %d\n", 1, 1, 2);
 
 	HIT_GOOD_TRAP;
 	return 0;
