a924117f57796946aa77e5b511a3ff57d95d9b7f
tracer-ics2015 <tracer@njuics.org>
2016-03-16 15:44:18 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  15:44:18 up 2 days,  8:16,  2 users,  load average: 0.21, 0.16, 0.11 f76b1fd9803741f7630ab8c8bf4730ba52c515a2

--- nemu/src/cpu/exec/misc/int.c
+++ nemu/src/cpu/exec/misc/int.c
@@ -16,7 +16,7 @@ void raise_intr(uint8_t NO) {
 	fflush(stdout);
 	gate.item=lnaddr_read(address,4)+((uint64_t)lnaddr_read(address+4,4)<<32);
 	uint32_t offset=gate.offset_15_0+(gate.offset_31_16<<16);
-	printf("%x\n",offset);
+	printf("%x\n",gate.segment);
 	fflush(stdout);
 	cpu.eip=swaddr_read(offset,4,gate.segment);
 	/* Jump back to cpu_exec() */
