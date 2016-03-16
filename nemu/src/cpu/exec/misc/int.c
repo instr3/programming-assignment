c08f104dfbb9d79b8289ca2e638433d159678c12
tracer-ics2015 <tracer@njuics.org>
2016-03-16 16:20:12 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:20:12 up 2 days,  8:25,  2 users,  load average: 0.06, 0.14, 0.13 1a77a65c7cf6a1f5b4ca71d46344e1daa1eb131b

--- nemu/src/cpu/exec/misc/int.c
+++ nemu/src/cpu/exec/misc/int.c
@@ -12,8 +12,8 @@ void raise_intr(uint8_t NO) {
 	idtitem_t gate;
 	lnaddr_t address=(uint32_t)NO*8+cpu.idtr_base;
 	assert(address<cpu.idtr_limit+cpu.idtr_base);
-	printf("%x\n",address);
-	fflush(stdout);
+	//printf("%x\n",address);
+	//fflush(stdout);
 	gate.item=lnaddr_read(address,4)+((uint64_t)lnaddr_read(address+4,4)<<32);
 	uint32_t offset=gate.offset_15_0+(gate.offset_31_16<<16);
 
@@ -28,8 +28,8 @@ void raise_intr(uint8_t NO) {
 	//inv->limit=gdt.seg_limit_0_15+((uint32_t)gdt.seg_limit_16_19<<16);
 
 
-	printf("%x\n",gate.segment);
-	fflush(stdout);
+	//printf("%x\n",gate.segment);
+	//fflush(stdout);
 	cpu.eip=base+offset;
 	/* Jump back to cpu_exec() */
 	longjmp(jbuf, 1);
@@ -37,7 +37,7 @@ void raise_intr(uint8_t NO) {
 
 make_helper(int_i) {
 	uint8_t id = instr_fetch(eip + 1, 1);
-	raise_intr(id);
 	print_asm("int $0x%X", id);
+	raise_intr(id);
 	return 2;
 }
