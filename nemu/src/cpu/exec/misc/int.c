46f76a2a5a5f2104b6650daeaaf8e0df70fdcc85
tracer-ics2015 <tracer@njuics.org>
2016-03-16 16:18:32 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:18:32 up 2 days,  8:23,  2 users,  load average: 0.21, 0.18, 0.14 dc2daca3ed18aee77ae7080c3c58badaab4e66b8

--- nemu/src/cpu/exec/misc/int.c
+++ nemu/src/cpu/exec/misc/int.c
@@ -16,9 +16,21 @@ void raise_intr(uint8_t NO) {
 	fflush(stdout);
 	gate.item=lnaddr_read(address,4)+((uint64_t)lnaddr_read(address+4,4)<<32);
 	uint32_t offset=gate.offset_15_0+(gate.offset_31_16<<16);
-	printf("%x\n",gate.segment>>2);
+
+
+	segmentselector_t seg;
+	seg.selector=gate.segment;
+	gdtitem_t gdt;
+	address=(uint32_t)seg.index*8+cpu.gdtr_base;
+	assert(address<cpu.gdtr_limit+cpu.gdtr_base);
+	gdt.item=lnaddr_read(address,4)+((uint64_t)lnaddr_read(address+4,4)<<32);
+	uint32_t base=gdt.seg_base_0_15+((uint32_t)gdt.seg_base_16_23<<16)+((uint32_t)gdt.seg_base_24_31<<24);
+	//inv->limit=gdt.seg_limit_0_15+((uint32_t)gdt.seg_limit_16_19<<16);
+
+
+	printf("%x\n",gate.segment);
 	fflush(stdout);
-	cpu.eip=swaddr_read(offset,4,gate.segment);
+	cpu.eip=base+offset;
 	/* Jump back to cpu_exec() */
 	longjmp(jbuf, 1);
 }
