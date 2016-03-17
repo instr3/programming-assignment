6986a2d034d36eb1fd5f9f886201524cdcae6623
tracer-ics2015 <tracer@njuics.org>
2016-03-17 17:53:45 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  17:53:45 up 3 days,  3:52,  2 users,  load average: 0.04, 0.04, 0.06 3220526e9ccc1841fb6cea2625b71d59ea8c5f11

--- nemu/src/cpu/exec/misc/clisti.c
+++ nemu/src/cpu/exec/misc/clisti.c
@@ -2,10 +2,12 @@
 
 make_helper(cli) {
 	reg_flag_set(EFLAGS_IF,0);
+	print_asm("cli");
 	return 1;
 }
 
 make_helper(sti) {
 	reg_flag_set(EFLAGS_IF,1);
+	print_asm("sti");
 	return 1;
 }
--- nemu/src/cpu/exec/string/cldstd.c
+++ nemu/src/cpu/exec/string/cldstd.c
@@ -2,19 +2,23 @@
 
 make_helper(cld) {
 	reg_flag_set(EFLAGS_DF,0);
+	print_asm("cld");
 	return 1;
 }
 
 make_helper(std) {
 	reg_flag_set(EFLAGS_DF,1);
+	print_asm("std");
 	return 1;
 }
 make_helper(clc) {
 	reg_flag_set(EFLAGS_CF,0);
+	print_asm("clc");
 	return 1;
 }
 
 make_helper(stc) {
 	reg_flag_set(EFLAGS_CF,1);
+	print_asm("stc");
 	return 1;
 }
