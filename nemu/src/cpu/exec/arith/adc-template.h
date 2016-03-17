0f5eb38c27998ed4d7c58d9705c26b28ec878d23
tracer-ics2015 <tracer@njuics.org>
2016-03-17 16:55:13 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:55:13 up 3 days,  2:54,  2 users,  load average: 0.04, 0.08, 0.11 e7618ae2c0e1633c53f41914cbb80057cc376ad7

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -3,6 +3,7 @@
 #define instr adc
 
 static void do_execute () {
+	assert(false);
 	DATA_TYPE result = op_dest->val + op_src->val + reg_flag(EFLAGS_CF);
 	OPERAND_W(op_dest, result);
 	influence_zf(result);
