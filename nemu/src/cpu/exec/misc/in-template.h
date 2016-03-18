3eb400102e7fff29c2bebc61d2e360077cd0c5ea
tracer-ics2015 <tracer@njuics.org>
2016-03-18 15:22:50 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  15:22:50 up 3 days,  8:11,  2 users,  load average: 0.13, 0.14, 0.13 a7f146ec555979b39647aba2b420f7fdeb91cb1

--- nemu/src/cpu/exec/misc/in-template.h
+++ nemu/src/cpu/exec/misc/in-template.h
@@ -3,14 +3,14 @@
 extern uint32_t pio_read(ioaddr_t addr, size_t len);
 make_helper(concat(in_, SUFFIX)){
 	REG(R_EAX)=pio_read(reg_w(R_EDX),DATA_BYTE);
-	printf("in" str(SUFFIX) " %%%s, (%%dx)", REG_NAME(R_EAX));
-	print_asm("in" str(SUFFIX) " %%%s, (%%dx)", REG_NAME(R_EAX));
+	printf("in" str(SUFFIX) " (%%dx), %%%s\n", REG_NAME(R_EAX));
+	print_asm("in" str(SUFFIX) " (%%dx), %%%s", REG_NAME(R_EAX));
 	return 1;
 }
 make_helper(concat(in_i_, SUFFIX)){
 	uint8_t addr=instr_fetch(eip+1,1);
 	REG(R_EAX)=pio_read(addr,DATA_BYTE);
-	print_asm("in" str(SUFFIX) " %%%s, $0x%X", REG_NAME(R_EAX), addr);
+	print_asm("in" str(SUFFIX) " $0x%X, %%%s", addr, REG_NAME(R_EAX));
 	return 2;
 }
 
