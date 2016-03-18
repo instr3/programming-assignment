b6b6ba3d9aab56d5bf49137dc88ac70c9857dc64
tracer-ics2015 <tracer@njuics.org>
2016-03-18 19:02:44 +0800

    >  compile kernel 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  19:02:44 up 3 days, 11:04,  3 users,  load average: 0.26, 0.15, 0.14 decd17fa9bc5637097134791a9056df9a0b4cf66

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -42,7 +42,7 @@ void irq_handle(TrapFrame *tf) {
 	} else if (irq < 1000) {
 		panic("Unexpected exception #%d at eip = %x", irq, tf->eip);
 	} else if (irq >= 1000) {
-		printf("Encountered:%d\n",irq);
+		Log("Encountered:%d\n",irq);
 		int irq_id = irq - 1000;
 		assert(irq_id < NR_HARD_INTR);
 		struct IRQ_t *f = handles[irq_id];
