91a18add1f41ca31ab750f1b9531b1122e217eac
tracer-ics2015 <tracer@njuics.org>
2016-03-18 17:11:10 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  17:11:10 up 3 days,  9:59,  4 users,  load average: 0.21, 0.22, 0.26 f7ceb6cffeb72027573c2239cfee96aad5151e7a

--- nemu/src/cpu/exec/misc/int.c
+++ nemu/src/cpu/exec/misc/int.c
@@ -9,7 +9,7 @@ void raise_intr(uint8_t NO) {
 	push_data(cpu.eflags,4);
 	push_data(cpu.cs,4);
 	reg_flag_set(EFLAGS_IF,0);
-	push_data(cpu.eip+2,4);
+	push_data(cpu.eip,4);
 
 	idtitem_t gate;
 	lnaddr_t address=(uint32_t)NO*8+cpu.idtr_base;
@@ -39,8 +39,9 @@ make_helper(int_i) {
 	//for(i=0;i<10;++i)
 	//printf("[%c]\n",(char)swaddr_read(cpu.ecx+i,1,SREG_DS));
 	uint8_t id = instr_fetch(eip + 1, 1);
+	cpu.eip+=2;
 	raise_intr(id);
 	//Well, can't process to here, sadly.
 	print_asm("int $0x%X", id);
-	return 2;
+	return 0;
 }
