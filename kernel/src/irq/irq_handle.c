a30213f2f1ea334a11dea711416f9c287b359f32
tracer-ics2015 <tracer@njuics.org>
2016-03-18 19:09:01 +0800

    >  compile game 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  19:09:01 up 3 days, 11:10,  3 users,  load average: 0.09, 0.29, 0.25 4dd93317ecc98365df7ed69de5ce9fdc679fbef

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -32,13 +32,11 @@ void irq_handle(TrapFrame *tf) {
 	//panic("Have you re-organized the ``TrapFrame'' structure?");
 
 	int irq = tf->irq;
-
+	if (irq == 0xFF) irq=1001;
 	if (irq < 0) {
 		panic("Unhandled exception!");
 	} else if (irq == 0x80) {
 		do_syscall(tf);
-	} else if (irq == 0xFF) { //255
-
 	} else if (irq < 1000) {
 		panic("Unexpected exception #%d at eip = %x", irq, tf->eip);
 	} else if (irq >= 1000) {
