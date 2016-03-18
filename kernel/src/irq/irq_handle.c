6744751437202a3b01a313820265b7a02f125dfe
tracer-ics2015 <tracer@njuics.org>
2016-03-18 19:02:32 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  19:02:32 up 3 days, 11:04,  3 users,  load average: 0.24, 0.14, 0.14 cf6e9df6be0729e541b4d378ace043cdc46f3c70

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -37,9 +37,12 @@ void irq_handle(TrapFrame *tf) {
 		panic("Unhandled exception!");
 	} else if (irq == 0x80) {
 		do_syscall(tf);
+	} else if (irq == 0xFF) { //255
+
 	} else if (irq < 1000) {
 		panic("Unexpected exception #%d at eip = %x", irq, tf->eip);
 	} else if (irq >= 1000) {
+		printf("Encountered:%d\n",irq);
 		int irq_id = irq - 1000;
 		assert(irq_id < NR_HARD_INTR);
 		struct IRQ_t *f = handles[irq_id];
