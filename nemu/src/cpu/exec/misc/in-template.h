4c2e77960fa45db891b3779bc8def0f0c54b8a4e
tracer-ics2015 <tracer@njuics.org>
2016-03-18 15:19:37 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  15:19:37 up 3 days,  8:08,  2 users,  load average: 0.09, 0.16, 0.13 5a198773861f3d48dddd8832a433faf9ba976e

--- nemu/src/cpu/exec/misc/in-template.h
+++ nemu/src/cpu/exec/misc/in-template.h
@@ -9,6 +9,7 @@ make_helper(concat(in_, SUFFIX)){
 make_helper(concat(in_i_, SUFFIX)){
 	uint8_t addr=instr_fetch(eip+1,1);
 	REG(R_EAX)=pio_read(addr,DATA_BYTE);
+	printf("in" str(SUFFIX) " %%%s, $0x%X", REG_NAME(R_EAX), addr);
 	print_asm("in" str(SUFFIX) " %%%s, $0x%X", REG_NAME(R_EAX), addr);
 	return 2;
 }
