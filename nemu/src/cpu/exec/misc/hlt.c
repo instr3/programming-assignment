b48d7c5990388cebc1433a5bdc0b68d9700f4050
tracer-ics2015 <tracer@njuics.org>
2016-03-18 17:12:48 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  17:12:48 up 3 days, 10:01,  4 users,  load average: 0.93, 0.46, 0.34 85c5d86363d8f24935175243393801e77b9547b9

--- nemu/src/cpu/exec/misc/hlt.c
+++ nemu/src/cpu/exec/misc/hlt.c
@@ -2,9 +2,7 @@
 extern uint8_t i8259_query_intr();
 make_helper(hlt)
 {
-	while(!(cpu.INTR & reg_flag(EFLAGS_IF)));
-	//printf("intr:%x",cpu.INTR);
-	//printf("flag:%x\n",reg_flag(EFLAGS_IF));
 	print_asm("hlt");
+	if(!(cpu.INTR & reg_flag(EFLAGS_IF)))return 0;
 	return 1;
 }
