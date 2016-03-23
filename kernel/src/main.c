9912058d4ece0c5e5381ea1d36a8c81e4114abad
tracer-ics2015 <tracer@njuics.org>
2016-03-23 15:36:16 +0800

    >  compile kernel 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  15:36:16 up 4 days,  1:05,  2 users,  load average: 0.31, 0.30, 0.26 322d1036e6ffcffd10a95121d3a5ced27153faa1

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -101,8 +101,8 @@ void init_cond() {
 	asm volatile("movl $0, %ebp");
 	asm volatile("subl $16, %esp");
 	
-	set_bp();
 	Log("%x\n",eip);
+	set_bp();
 	/* Here we go! */
 	((void(*)(void))eip)();
 
