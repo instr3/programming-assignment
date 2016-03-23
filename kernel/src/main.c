ca528f65b443aa05b20dcff642f1b97098e601be
tracer-ics2015 <tracer@njuics.org>
2016-03-23 17:50:49 +0800

    >  compile kernel 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  17:50:49 up 4 days,  2:55,  2 users,  load average: 0.29, 0.24, 0.25 c880046975c2b109233326bffb0cc7c72df6bc5

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -35,7 +35,7 @@ void init() {
 
 	panic("should not reach here");
 }
-
+uint32_t eip;
 /* Initialization phase 2 */
 void init_cond() {
 #ifdef IA32_INTR
@@ -79,7 +79,7 @@ void init_cond() {
 #endif
 
 	/* Load the program. */
-	uint32_t eip = loader();
+	eip = loader();
 	
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Read data in the video memory to check whether 
