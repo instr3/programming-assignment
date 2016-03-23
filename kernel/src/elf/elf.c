5b741bbf2fdb2673c304cd976aa05816076de5d7
tracer-ics2015 <tracer@njuics.org>
2016-03-23 15:13:39 +0800

    >  compile kernel 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  15:13:39 up 4 days, 42 min,  2 users,  load average: 0.09, 0.18, 0.18 c1387680bcb2d12fbc8e2ee3535360467504ea8f

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -43,10 +43,10 @@ uint32_t loader() {
 			//Physical memory and Virtual memory pointed to the same page
 			//So (void *)((hwaddr)) or (void *)(pa_to_va(hwaddr)) all works in memcpy and memset.
 #ifdef HAS_DEVICE
-			
+
 			ide_read((void *)((hwaddr)), ph->p_offset, ph->p_filesz);
 #else
-			memcpy((void *)((hwaddr)),(void *)(ph->p_offset),ph->p_filesz);
+			//memcpy((void *)((hwaddr)),(void *)(ph->p_offset),ph->p_filesz);
 #endif
 			memset((void *)((hwaddr)+ph->p_filesz),0,ph->p_memsz-ph->p_filesz);
 
