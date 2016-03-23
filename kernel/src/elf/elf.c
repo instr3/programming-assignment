05dda472446616b886aefed9ccd0f6f7b6db195e
tracer-ics2015 <tracer@njuics.org>
2016-03-23 16:15:40 +0800

    >  compile kernel 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:15:40 up 4 days,  1:19,  2 users,  load average: 0.14, 0.15, 0.20 ca5a322945a13e99ea942aa3c267bc4a484cf6ba

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -15,7 +15,7 @@ void ramdisk_read(uint8_t *, uint32_t, uint32_t);
 void create_video_mapping();
 uint32_t get_ucr3();
 
-uint32_t loader() {
+volatile uint32_t loader() {
 	Elf32_Ehdr *elf;
 	Elf32_Phdr *ph = NULL;
 
@@ -32,7 +32,7 @@ uint32_t loader() {
 	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	//set_bp();
-	Log("%x\n",*p_magic);
+	//Log("%x\n",*p_magic);
 	nemu_assert(*p_magic == elf_magic);
 	//set_bp();
 
