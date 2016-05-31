3954c1fc8078b5616e579f457ca19840fcd774f9
tracer-ics2015 <tracer@njuics.org>
2016-05-31 13:55:13 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  13:55:13 up 1 day,  8:09,  2 users,  load average: 0.10, 0.12, 0.13 6f3cdaf87184bcaef2466c44198b4dbe7c41fba

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -3,7 +3,6 @@
 #include "memory/page.h"
 #include "memory/segment.h"
 #include "device/mmio.h"
-//uint8_t simple_memory[1<<27];
 //#define swaddr_read simple_read
 //#define swaddr_write simple_write
 uint32_t simple_read(hwaddr_t addr, size_t len) {
@@ -22,7 +21,6 @@ void simple_write(swaddr_t addr, size_t len, uint32_t data) {
 	if(map_no!=-1){mmio_write(addr,len,data,map_no);return;}
 	printf("w %x,%u\n",addr,(unsigned)len);
 	assert(addr<(1<<27));
-	hwaddr_write(addr,len,data);return;
 	if(len==4){*(uint32_t *)(hw_mem+addr)=data;return;}
 	if(len==1){hw_mem[addr]=data;return;}
 	*(uint16_t *)(hw_mem+addr)=data;
