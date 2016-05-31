a831b8f15e5b1cba1da652b8c2aadee4df9fbe6b
tracer-ics2015 <tracer@njuics.org>
2016-05-31 11:02:58 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  11:02:58 up 1 day,  5:49,  2 users,  load average: 0.16, 0.53, 0.46 17ed880e90ba97581005bf11b5c6d00d7473751c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -8,18 +8,19 @@ uint8_t simple_memory[1<<23];
 //#define swaddr_write simple_write
 uint32_t simple_read(hwaddr_t addr, size_t len) {
 	if(addr>=0xC0000000)addr-=0xC0000000;
-	assert(addr<(1<<23));
 	int map_no=is_mmio(addr);
 	if(map_no!=-1)return mmio_read(addr,len,map_no);
+	printf("%d\n",addr);
+	assert(addr<(1<<23));
 	if(len==4)return *(uint32_t *)(simple_memory+addr);
 	if(len==1)return simple_memory[addr];
 	return *(uint16_t *)(simple_memory+addr);
 }
 void simple_write(swaddr_t addr, size_t len, uint32_t data) {
 	if(addr>=0xC0000000)addr-=0xC0000000;
-	assert(addr<(1<<23));
 	int map_no=is_mmio(addr);
 	if(map_no!=-1){mmio_write(addr,len,data,map_no);return;}
+	assert(addr<(1<<23));
 	if(len==4)*(uint32_t *)(simple_memory+addr)=data;
 	if(len==1)simple_memory[addr]=data;
 	*(uint16_t *)(simple_memory+addr)=data;
