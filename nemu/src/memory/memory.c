eb62b62ae5f2f2ce476ce2791970d1910441d72d
tracer-ics2015 <tracer@njuics.org>
2016-05-31 14:17:08 +0800

    >  compile NEMU 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  14:17:08 up 1 day,  8:31,  2 users,  load average: 0.20, 0.25, 0.31 b1ec125a95d856ddadf88bf9d8614b4f3bbee

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -82,6 +82,7 @@ void debug_cache_address(hwaddr_t addr)
 	}
 	uint32_t lnaddr_read(hwaddr_t addr,size_t len){
 		if(addr>=0xC0000000)addr-=0xC0000000;
+		assert(addr<HW_MEM_SIZE);
 		int map_no=is_mmio(addr);
 		if(map_no!=-1)return mmio_read(addr,len,map_no);
 		if(len==4)return *(uint32_t *)(hw_mem+addr);
@@ -90,6 +91,7 @@ void debug_cache_address(hwaddr_t addr)
 	}
 	void lnaddr_write(swaddr_t addr, size_t len, uint32_t data) {
 		if(addr>=0xC0000000)addr-=0xC0000000;
+		assert(addr<HW_MEM_SIZE);
 		int map_no=is_mmio(addr);
 		if(map_no!=-1){mmio_write(addr,len,data,map_no);return;}
 		if(len==4){*(uint32_t *)(hw_mem+addr)=data;return;}
