123dccd92a7b49604b8e2044188d3ee280a53470
tracer-ics2015 <tracer@njuics.org>
2016-06-11 16:11:10 +0800

    >  compile game 14307130166 jjy-vm Linux pass123456-virtual-machine 3.13.0-37-generic #64-Ubuntu SMP Mon Sep 22 21:28:38 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:11:10 up 1 day,  9:53,  2 users,  load average: 0.77, 0.66, 0.55 39520d455336b68d652103c61ce75167dee430eb

--- game/src/nemu-pal/hal/keyboard.c
+++ game/src/nemu-pal/hal/keyboard.c
@@ -26,7 +26,7 @@ keyboard_event(void) {
 			if(keycode_array[i]==key_code)
 			{
 				Log("Current:%x\n",key_state[i]);
-				key_state[i]=KEY_STATE_WAIT_RELEASE;
+				key_state[i]=KEY_STATE_PRESS;
 				return;
 			}
 		}
